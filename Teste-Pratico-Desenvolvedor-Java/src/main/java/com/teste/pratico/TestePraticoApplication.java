c0873fab86a2ef24f3cf7627769eb063b418a5f4
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-09-27 20:34:53 -0300

    init: initial project structure with Maven and basic configuration

---
+++ Teste-Pratico-Desenvolvedor-Java/src/main/java/com/teste/pratico/TestePraticoApplication.java
@@ -0,0 +1,17 @@
+package com.teste.pratico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.teste.pratico"}) 
+public class TestePraticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePraticoApplication.class, args);
+	}
+
+}
