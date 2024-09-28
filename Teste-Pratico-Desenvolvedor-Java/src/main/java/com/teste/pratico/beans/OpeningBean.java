4ec5511409bc17a623cd07b548c6734e46e26ece
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-09-28 15:07:12 -0300

    feat(bean): create iniital class for bean opening

---
+++ Teste-Pratico-Desenvolvedor-Java/src/main/java/com/teste/pratico/beans/OpeningBean.java
@@ -0,0 +1,13 @@
+package com.teste.pratico.beans;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.ManagedBean;
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+
+@ManagedBean
+@ViewScoped
+@Component
+public class OpeningBean implements Serializable {
+}
